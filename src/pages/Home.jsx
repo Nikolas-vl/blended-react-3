a53e28984daf1361026d4877659d20915e07c72d
Nikolas-vl <nikvv02@gmail.com>
2025-05-05 13:07:24 +0200

    done practice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
 import Container from '../components/Container/Container';
 import Heading from '../components/Heading/Heading';
 import Section from '../components/Section/Section';
+import { getCountries } from '../service/countryApi';
+import Loader from '../components/Loader/Loader';
+import CountryList from '../components/CountryList/CountryList';
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const data = await getCountries();
+        setCountries(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
+        {isLoading && <Loader />}
+        {error && <Heading title="oops..." bottom></Heading>}
+        {countries.length > 0 && <CountryList countries={countries} />}
         <Heading title="Home" bottom />
       </Container>
     </Section>
